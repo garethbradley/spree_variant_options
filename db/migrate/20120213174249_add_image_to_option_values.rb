be55c8330769a4d00c1c4df7fe2fed40e3d927ee
Gareth Bradley <gb@garethbradley.co.uk>
2014-12-23 17:09:46 +0000

    Update 20120213174249_add_image_to_option_values.rb

--- db/migrate/20120213174249_add_image_to_option_values.rb
+++ db/migrate/20120213174249_add_image_to_option_values.rb
@@ -1,5 +1,7 @@
 class AddImageToOptionValues < ActiveRecord::Migration
   def change
+    add_column :spree_option_values, :image_width,        :integer
+    add_column :spree_option_values, :image_height,       :integer
     add_column :spree_option_values, :image_file_name,    :string
     add_column :spree_option_values, :image_content_type, :string
     add_column :spree_option_values, :image_file_size,    :integer

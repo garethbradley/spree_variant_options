80e266d1da208076025a289d3f73d79edb501ec3
mrkre <kreene@yahoo.com>
2015-01-23 17:06:09 +0800

    Removed dependency on spree_frontend

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -7,13 +7,4 @@ end
 # Copy spree user initializer
 template "initializers/spree_user.rb", "config/initializers/spree_user.rb"
 
-# remove all stylesheets except core
-%w(backend frontend).each do |ns|
-  template "spree/#{ns}/all.js",  "app/assets/javascripts/spree/#{ns}/all.js",  :force => true
-  template "spree/#{ns}/all.css", "app/assets/stylesheets/spree/#{ns}/all.css", :force => true
-end
-
-# Fix sass load error by using the converted css file
-template "spree/frontend/screen.css", "app/assets/stylesheets/spree/frontend/screen.css"
-
 run "rails g spree_variant_options:install"
